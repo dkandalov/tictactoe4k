768e9f75166f4bab60ee43d743b00b2627b39123
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-12-02 12:50:32 +0100

    live coding

--- src/test/Main.kt
+++ src/test/Main.kt
@@ -1,5 +1,4 @@
 
-import Player.*
 import org.http4k.client.*
 import org.http4k.core.*
 import org.http4k.core.Method.*
@@ -8,7 +7,7 @@ import org.http4k.core.Status.Companion.SEE_OTHER
 import org.http4k.filter.ClientFilters.SetBaseUriFrom
 import org.http4k.filter.DebuggingFilters.PrintRequestAndResponse
 import org.http4k.filter.ServerFilters.CatchAll
-import org.http4k.format.Moshi.autoBody
+import org.http4k.format.*
 import org.http4k.lens.*
 import org.http4k.routing.*
 import org.http4k.server.*
@@ -16,20 +15,18 @@ import org.http4k.template.*
 import java.util.concurrent.atomic.*
 
 fun main() {
-    newBackend(Game()).asServer(ApacheServer(port = 1234)).start()
+    newBackend().asServer(ApacheServer(port = 1234)).start()
 
     val backendClient = SetBaseUriFrom(Uri.of("http://localhost:1234")).then(OkHttp())
     newFrontend(backendClient).asServer(ApacheServer(port = 8080)).start()
-
-    println("Started on http://localhost:8080")
 }
 
 fun newFrontend(backend: HttpHandler): HttpHandler {
     val htmlRenderer = HandlebarsTemplates().HotReload("src/test")
+
     return routes(
         "/" bind GET to {
-            val response = backend(Request(GET, "/game"))
-            val game = gameLens(response)
+            val game = gameLens(backend(Request(GET, "/game")))
             Response(OK).body(htmlRenderer(game.toGameView()))
         },
         "/move/{x}/{y}" bind GET to { request ->
@@ -64,12 +61,16 @@ private fun Game.toGameView() = GameView(
     winner = winner?.name
 )
 
-val gameLens = autoBody<Game>().toLens()
+
+
+val gameLens = Moshi.autoBody<Game>().toLens()
 val xLens = Query.int().required("x")
 val yLens = Query.int().required("y")
 
-fun newBackend(initialGame: Game): HttpHandler {
+
+fun newBackend(initialGame: Game = Game()): HttpHandler {
     val game = AtomicReference(initialGame)
+
     return routes(
         "/game" bind GET to {
             gameLens.inject(game.get(), Response(OK))
@@ -85,26 +86,28 @@ fun newBackend(initialGame: Game): HttpHandler {
     ).withFilter(PrintRequestAndResponse().then(CatchAll()))
 }
 
+
 data class Game(val moves: List<Move> = emptyList()) {
     val winner: Player? = findWinner()
 
     fun makeMove(x: Int, y: Int): Game {
         if (winner != null) return this
-        val nextPlayer = if (moves.lastOrNull()?.player != X) X else O
+        val nextPlayer = if (moves.lastOrNull()?.player == Player.X) Player.O else Player.X
         return copy(moves = moves + Move(x, y, nextPlayer))
     }
 
     private fun findWinner(): Player? =
         enumValues<Player>().find { player ->
-            moves.containsAll((0..2).map { Move(it, 0, player) }) ||
-            moves.containsAll((0..2).map { Move(it, 1, player) }) ||
-            moves.containsAll((0..2).map { Move(it, 2, player) }) ||
-            moves.containsAll((0..2).map { Move(0, it, player) }) ||
-            moves.containsAll((0..2).map { Move(1, it, player) }) ||
-            moves.containsAll((0..2).map { Move(2, it, player) }) ||
-            moves.containsAll((0..2).map { Move(it, it, player) }) ||
-            moves.containsAll((0..2).map { Move(it, 2 - it, player) })
+            moves.containsAll((0..2).map { Move(x = it, y = 0, player) }) ||
+            moves.containsAll((0..2).map { Move(x = it, y = 1, player) }) ||
+            moves.containsAll((0..2).map { Move(x = it, y = 2, player) }) ||
+            moves.containsAll((0..2).map { Move(x = 0, y = it, player) }) ||
+            moves.containsAll((0..2).map { Move(x = 1, y = it, player) }) ||
+            moves.containsAll((0..2).map { Move(x = 2, y = it, player) }) ||
+            moves.containsAll((0..2).map { Move(x = it, y = it, player) }) ||
+            moves.containsAll((0..2).map { Move(x = it, y = 2 - it, player) })
         }
+
 }
 
 data class Move(val x: Int, val y: Int, val player: Player)
--- src/test/Tests.kt
+++ src/test/Tests.kt
@@ -1,76 +1,62 @@
-
 import Player.*
 import datsok.*
 import org.http4k.core.*
 import org.http4k.core.Method.*
 import org.http4k.core.Status.Companion.OK
-import org.http4k.filter.ClientFilters.FollowRedirects
+import org.http4k.filter.*
 import org.http4k.testing.*
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.extension.*
 
 @ExtendWith(ApprovalTest::class)
 class FrontendTests {
-    private val frontend = FollowRedirects().then(
-        newFrontend(newBackend(Game()))
-    )
+    private val frontend =
+        ClientFilters.FollowRedirects().then(newFrontend(newBackend()))
 
     @Test fun `state of an empty game`(approver: Approver) {
         val response = frontend(Request(GET, "/")).expectOK()
         approver.assertApproved(response)
     }
 
-    @Test fun `players take turns on each move`(approver: Approver) {
+    @Test fun `players take turn on each move`(approver: Approver) {
         frontend(Request(GET, "/move/0/1")).expectOK()
-        frontend(Request(GET, "/move/2/0")).expectOK()
-        val response = frontend(Request(GET, "/move/1/1")).expectOK()
-        approver.assertApproved(response)
-    }
+        frontend(Request(GET, "/move/1/0")).expectOK()
+        frontend(Request(GET, "/move/2/2")).expectOK()
 
-    @Test fun `player X wins`(approver: Approver) {
-        val frontend = newFrontend(newBackend(finishedGame))
-        approver.assertApproved(frontend(Request(GET, "/")).expectOK())
+        val response = frontend(Request(GET, "/")).expectOK()
+        approver.assertApproved(response)
     }
 }
 
-
 class BackendTests {
-    private val backend = newBackend(Game())
+    private val backend = newBackend()
 
     @Test fun `state of an empty game`() {
         val response = backend(Request(GET, "/game")).expectOK()
         response.bodyString() shouldEqual "{\"moves\":[]}"
     }
 
-    @Test fun `players take turns on each move`() {
+    @Test fun `players take turn on each move`() {
         backend(Request(POST, "/game?x=0&y=1")).expectOK()
-        backend(Request(POST, "/game?x=2&y=0")).expectOK()
-        backend(Request(POST, "/game?x=1&y=1")).expectOK()
+        backend(Request(POST, "/game?x=1&y=0")).expectOK()
+        backend(Request(POST, "/game?x=2&y=2")).expectOK()
 
         val response = backend(Request(GET, "/game")).expectOK()
-        gameLens.extract(response) shouldEqual Game(moves = listOf(
-            Move(0, 1, X),
-            Move(2, 0, O),
-            Move(1, 1, X),
-        ))
+        gameLens.extract(response) shouldEqual Game(
+            listOf(
+                Move(0, 1, X),
+                Move(1, 0, O),
+                Move(2, 2, X),
+            )
+        )
     }
+}
 
-    @Test fun `player X wins`() {
-        val backend = newBackend(finishedGame)
-        gameLens(backend(Request(GET, "/game"))).winner shouldEqual X
-    }
+private fun Response.expectOK(): Response {
+    this.status shouldEqual OK
+    return this
 }
 
 class GameTests {
     // ...
-}
-
-private val finishedGame = Game()
-    .makeMove(0, 0).makeMove(1, 0)
-    .makeMove(0, 1).makeMove(1, 1)
-    .makeMove(0, 2)
-
-private fun Response.expectOK(): Response {
-    status shouldEqual OK
-    return this
 }
